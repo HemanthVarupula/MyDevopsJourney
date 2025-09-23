39d9e222d5d097a7938576426a433ebd827cfcd3
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-23 07:24:05 +0530

    added functions

---
+++ shell/function.sh
@@ -0,0 +1,6 @@
+read -p "enter num1" num1
+read -p "enter num2" num2
+add(){
+	echo "adding two numbers : $((num1+num2))"
+}
+add
---
+++ shell/mul.sh
@@ -0,0 +1,6 @@
+num1=$1
+num2=$2
+mul(){
+	echo "multiplying of two numbers is : $(($1*$2))"
+}
+mul $1 $2
